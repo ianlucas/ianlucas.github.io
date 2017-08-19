d5602655bd00a1566863c26d63aee8ef414056da
Ian Lucas <ianlucas_ferreira@hotmail.com>
2017-08-18 23:07:30 -0300

    New MathJax Editor page

--- mathjax-editor/scripts.js
+++ mathjax-editor/scripts.js
@@ -1,8 +1,4 @@
-// Hello there!
-
 window.onload = function () {
-  var editor = new MathJaxEditor({
-    el: '#myEditor',
-    value: 'x=\\frac{-b\\pm \\sqrt{b^{2}-4ac}}{2a}'
-  });
-};
+  window.editor = new MathJaxEditor('#editor')
+  window.editor.setValue('<math><mi>x</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>±</mo><msqrt><mrow><msup><mrow><mi>b</mi></mrow><mrow><mn>2</mn></mrow></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math>')
+}
