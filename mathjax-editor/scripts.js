a78a95fee505ac70736a0c5032567dd84c368ab6
Ian Lucas <ianlucas_ferreira@hotmail.com>
2018-06-30 23:26:07 -0300

    Fix initial download glitch on editor

--- mathjax-editor/scripts.js
+++ mathjax-editor/scripts.js
@@ -1,4 +1,6 @@
 window.onload = function () {
-  window.editor = new MathJaxEditor('#editor')
-  window.editor.setValue('<math><mi>x</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>±</mo><msqrt><mrow><msup><mrow><mi>b</mi></mrow><mrow><mn>2</mn></mrow></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math>')
+  MathJax.Hub.Register.StartupHook('TeX Jax Ready', function () {
+    window.editor = new MathJaxEditor('#editor')
+    window.editor.setValue('<math><mi>x</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>±</mo><msqrt><mrow><msup><mrow><mi>b</mi></mrow><mrow><mn>2</mn></mrow></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math>')
+  })
 }
