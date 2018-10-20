5e241d268481acfea2b035f57d8c7a6dc8712e97
Ian Lucas <ianlucas_ferreira@hotmail.com>
2018-10-20 00:01:57 -0300

    Add my pega utils

---
+++ pega-utils/precache-manifest.cf05da0d1985362535f1e6cc12fee4f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "471890281cbc219a2c53",
+    "url": "/static/js/main.47189028.chunk.js"
+  },
+  {
+    "revision": "53d59b6d310b6b3962f0",
+    "url": "/static/js/1.53d59b6d.chunk.js"
+  },
+  {
+    "revision": "471890281cbc219a2c53",
+    "url": "/static/css/main.2e8a1d0c.chunk.css"
+  },
+  {
+    "revision": "53d59b6d310b6b3962f0",
+    "url": "/static/css/1.38825878.chunk.css"
+  },
+  {
+    "revision": "14999c7338d956f9bed5dc7707d5e564",
+    "url": "/index.html"
+  }
+];
