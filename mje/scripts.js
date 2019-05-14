c0f1b16a3c0810f8d39a78b3fdd012311cc41019
Ian Lucas <ianlucas_ferreira@hotmail.com>
2019-05-14 02:02:54 -0300

    Add mje page

---
+++ mje/scripts.js
@@ -0,0 +1,2 @@
+window.editor = mje('#myEquation')
+window.editor.set('<math><mi>x</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>±</mo><msqrt><mrow editable><msup><mrow><mi>b</mi></mrow><mrow><mn>2</mn></mrow></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math>')
