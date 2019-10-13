8ba00eed56bc1871178a3f14bab8937a92561fbb
Ian Lucas <ianlucas_ferreira@hotmail.com>
2019-10-12 23:14:22 -0300

    Update Mathjax Editor page

--- mathjax-editor/scripts.js
+++ mathjax-editor/scripts.js
@@ -1,6 +1,2 @@
-window.onload = function () {
-  MathJax.Hub.Register.StartupHook('TeX Jax Ready', function () {
-    window.editor = new MathJaxEditor('#editor')
-    window.editor.setValue('<math><mi>x</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>±</mo><msqrt><mrow><msup><mrow><mi>b</mi></mrow><mrow><mn>2</mn></mrow></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math>')
-  })
-}
+window.editor = mje('#myEquation')
+window.editor.set('<math><mi>x</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>±</mo><msqrt><mrow editable><msup><mrow><mi>b</mi></mrow><mrow><mn>2</mn></mrow></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math>')
